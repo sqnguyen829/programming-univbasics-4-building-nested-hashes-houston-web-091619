9cec8c067e0f3aced166c767296b7e31d0fe0981
steven nguyen <sqnguyen829@gmail.com>
2019-09-12 02:43:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       rent_in_dollars: {
-        one_piece_owned: "25",
+        one_piece_owned: 25,
         two_pieces_owned: "50",
         three_pieces_owned: "100",
         four_pieces_owned: "200"
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
     railroads: {
       pieces: 4,
       rent_in_dollars: {
-        one_piece_owned: 25,
+        one_piece_owned: "25",
         two_pieces_owned: "50",
         three_pieces_owned: "100",
         four_pieces_owned: "200"
